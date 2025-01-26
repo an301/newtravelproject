b4dc1b5a64b8d72bc7e6743f7e430ed098f1566f
Anish <anish.nambi@gmail.com>
2025-01-25 16:52:37 -0800

    main page with interactive map

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MapComponent from "./Map";
+
+function App() {
+  return (
+    <div>
+      <h1>TripSync</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
